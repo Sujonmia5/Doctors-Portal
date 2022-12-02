ff273f9487def3d2ba41302bbed239c79fc3fb2a
Sujonmia5 <mdsujon673738@gmail.com>
2022-12-02 21:44:47 +0600

    github new connect

--- src/Shared/Spinner.js
+++ src/Shared/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner = () => {
     return (
         <div className='flex justify-center items-center'>
-            <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin dark:border-primary"></div>
+            <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin dark:border-primary "></div>
         </div>
     );
 };
